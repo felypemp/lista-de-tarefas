7320646d455c81777671158b815ee2b96caa9cf0
Felipe <fmp.ti@outlook.com>
2023-08-05 10:35:53 -0300

    Criando funções para salvar e apagar tarefas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,61 @@ const btnTarefa = document.querySelector('.btn-tarefa');
 const listaTarefa = document.querySelector('.tarefas');
 
 function CriaLi(){
-    
+    const li = document.createElement('li');
+    return li;
+}
+
+document.addEventListener( 'keypress', function(e) {
+    if (e.keyCode === 13){
+        if (!inputTarefa.value) return;
+        adicionaTarefa(inputTarefa.value) 
+    }
+});
+
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class','apagar');
+    li.appendChild(botaoApagar);
 }
 
 btnTarefa.addEventListener('click', () => {
     if (!inputTarefa.value) return;
     adicionaTarefa(inputTarefa.value);
-})
+});
 
 function adicionaTarefa(textoInput){
-    listaTarefa.innerHTML = textoInput;
+    const li = CriaLi();
+    li.innerText = textoInput;
+    listaTarefa.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefa();
+};
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    if(el.classList.contains('apagar')){
+        el.parentNode.remove()
+    }
+})
+
+
+function salvarTarefa() {
+    const liTarefas = listaTarefa.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim;
+        console.log(tarefaTexto)
+        listaDeTarefas.push(tarefaTexto)
+    }
 }
+
